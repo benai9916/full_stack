85ea31748ca0f7976b4ff1471d56f34137213dc3
benai9916 <benaikumar2@gmail.com>
2022-10-07 20:32:25 +0530

    add vercel url

--- aerialops/src/pages/_app.tsx
+++ aerialops/src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+      : "https://aerialops-ruby.vercel.app/api/trpc";
     return {
       url,
       transformer: superjson,
